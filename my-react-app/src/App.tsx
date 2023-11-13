71b022984c85a8dd89b862fe558f080ddd9cb101
CrynesSs <j.warttmann@gmx.de>
2023-11-13 06:30:07 +0100

    I regret everything

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from "./Home";
-import ClientList from "./ClientList";
+import ClientList from "./components/usergui/ClientList";
+import AddClientForm from "./components/usergui/AddClientForm";
+import EditClientForm from "./components/usergui/EditClientForm";
 
 const App: React.FC = ()=>{
     return (
@@ -10,6 +12,8 @@ const App: React.FC = ()=>{
             <Routes>
                 <Route path='/' element={<Home/>}/>
                 <Route path='/clients' element={<ClientList/>}/>
+                {/*<Route path="/clients/new" element={<AddClientForm isOpen onAdd={} onClose={}/>} />
+                <Route path="/clients/edit/:id" element={<EditClientForm initialData={}/>} />*/}
             </Routes>
         </Router>
     )
--- my-react-app/src/components/usergui/ClientList.tsx
+++ my-react-app/src/components/usergui/ClientList.tsx
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import {Button, ButtonGroup, Container} from 'reactstrap';
 import {Link, useNavigate} from 'react-router-dom';
-import AppNavbar from "./AppNavBar";
-interface Client {
+import AppNavbar from "../../AppNavBar";
+import EditClientForm from "./EditClientForm";
+export interface Client {
     id: React.Key;
     username: string;
     email: string;
@@ -11,12 +12,14 @@ interface Client {
 const ClientList: React.FC = () => {
     const navigate = useNavigate();
     const [clients, setClients] = useState<Client[]>([]);
+    const [editClient, setEditClient] = useState<Client | null>(null);
 
     useEffect(() => {
         fetch('/api/users')
             .then((response) => response.json())
             .then((data) => setClients(data));
     }, []);
+
 
     const remove = async (id: React.Key) => {
         await fetch(`/api/users/${id}`, {
@@ -30,17 +33,33 @@ const ClientList: React.FC = () => {
             setClients(updatedClients);
         });
     };
-
-    const handleEditClick = (id: React.Key) => {
-        navigate(`/clients/${id}`);
+    const handleEditClick = (client: Client) => {
+        setEditClient(client);
+        // Navigate to the edit form route
+        navigate(`/clients/edit/${client.id}`);
     };
 
+    const handleEditFormSubmit = (editedClient: Client) => {
+        // Handle the submission of the edited client data
+        // For simplicity, this example just sets the edited client data locally
+        const updatedClients = clients.map(client =>
+            client.id === editedClient.id ? { ...client, ...editedClient } : client
+        );
+        setClients(updatedClients);
+        setEditClient(null);
+    };
+
+    const handleEditFormClose = () => {
+        setEditClient(null);
+        // Navigate back to the client list
+        navigate('/clients');
+    };
     const clientList = clients.map((client) => (
         <div key={client.id} className="client-card">
             <div className="client-field">{client.username}</div>
             <div className="client-field">{client.email}</div>
             <ButtonGroup className={"client-field"}>
-                <Button color="primary" onClick={() => handleEditClick(client.id)}>
+                <Button color="primary" onClick={() => navigate("/clients/new")}>
                     Edit
                 </Button>
                 <Button color="danger" onClick={() => remove(client.id)}>
@@ -70,6 +89,15 @@ const ClientList: React.FC = () => {
                     </div>
                 </div>
                 <div className="client-list">{clientList}</div>
+                {/* EditClientForm component */}
+                {editClient && (
+                    <EditClientForm
+                        isOpen={true}
+                        initialData={editClient}
+                        onSave={handleEditFormSubmit}
+                        onClose={handleEditFormClose}
+                    />
+                )}
             </Container>
         </div>
     );

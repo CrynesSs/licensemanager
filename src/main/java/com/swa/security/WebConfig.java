e6ad77eb207a4b93ff4c9fde3093aa08432784e6
CrynesSs <j.warttmann@gmx.de>
2023-12-11 02:24:20 +0100

    Should be done now. Working Login also from https app

--- src/main/java/com/swa/security/WebConfig.java
+++ src/main/java/com/swa/security/WebConfig.java
@@ -1,18 +1,11 @@
 package com.swa.security;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
-import org.springframework.web.servlet.resource.ResourceTransformer;
-import org.springframework.web.servlet.resource.ResourceTransformerChain;
-import org.springframework.web.servlet.resource.TransformedResource;
-
-import java.io.IOException;
 
 @Configuration
 @EnableWebMvc
@@ -27,6 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry
                 .addResourceHandler("/**")
                 .addResourceLocations("classpath:/public/")
+                .addResourceLocations("classpath:/public/static/")
                 .setCachePeriod(3600)
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver());
